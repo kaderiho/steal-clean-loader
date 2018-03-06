532729ff5c0fdf95505ab1416803ec897e4bd4d5
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2018-03-06 13:50:30 +0300

    Loader files structure changes

--- build/index.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var stealCleanLoader = require('.');
-
-test('Strip the comment tags: default params', function () {
-    expect(stealCleanLoader('//!steal-remove-start //!steal-remove-end')).toBe('');
-});
