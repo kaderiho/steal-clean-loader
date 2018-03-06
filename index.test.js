6bb2eb978f153cc3116697e00de8245096ad1637
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2018-03-06 14:05:04 +0300

    Updates for tests

--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ const stealCleanLoader = require('.');
 test('Strip the comment tags: default params', () => {
     expect(stealCleanLoader('//!steal-remove-start //!steal-remove-end')).toBe('');
 });
+
+test('Strip the comment tags: some custom string', () => {
+    expect(stealCleanLoader('//!steal-remove-start Some custom string of code there //!steal-remove-end')).toBe('');
+});
